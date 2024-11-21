6adce75aafccf5357391b0f2c07294e3da5db016
Cory Carte <7457451+corycarte@users.noreply.github.com>
2024-11-21 06:12:48 -0500

    day1 complete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { readInputFile } from "./utils/utils";
 import { day1 } from "./day1/day1";
-import * as figlet from 'figlet';
+import * as figlet from "figlet";
 
 let day = process.argv[2];
 
@@ -13,6 +13,7 @@ switch (day) {
     day1(input);
     break;
   case "day2":
+
     break;
   case undefined:
     console.log("No input given");
