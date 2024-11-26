5260d736581bb3696bdf2f952e5d20cd09ad4267
Cory Carte <7457451+corycarte@users.noreply.github.com>
2024-11-26 03:00:59 -0500

    Day 4 and 5 Complete

---
+++ src/day4/day4.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs';
+import * as crypto from 'crypto';
+
+import { ResultData } from '../models/resultData';
+import { readInputFile } from '../utils/utils';
+
+function mineForHash(input: string, algo: string = 'md5'): string {
+  const hasher = crypto.createHash(algo);
+
+  return hasher.update(input).digest('hex');
+}
+
+
+function part1(input: string, searchingFor: string, startAt: number = 0, stopAt: number = 100_000_000): number {
+  let i = startAt;
+
+  while (i < stopAt) {
+
+    const hash = mineForHash(`${input}${i}`);
+
+    const pref = hash.substring(0, searchingFor.length);
+
+    if (pref === searchingFor) {
+      return i;
+    }
+
+    i += 1;
+  }
+
+  return -1;
+}
+
+export function processData(inputFile: string): ResultData {
+  const inputData = readInputFile(inputFile).replace("\n", "");
+
+  let res: ResultData = { part1: null, part2: null };
+
+  res.part1 = part1(inputData, '00000');
+  res.part2 = part1(inputData, '000000', res.part1);
+
+  return res;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { readInputFile } from "./utils/utils";
 import { day1 } from "./day1/day1";
 import * as day2 from "./day2/day2";
 import * as day3 from "./day3/day3";
+import * as day4 from "./day4/day4";
+import * as day5 from "./days/day5";
 import { ResultData } from "./models/resultData";
 
 const day = process.argv[2];
 const fileLoc = `./src/inputs/${day}`;
 
-let results: ResultData = { part1: 0, part2: 0 };
+let results: ResultData = { part1: null, part2: null };
 switch (day) {
   case "day1":
     day1(readInputFile(fileLoc));
@@ -17,6 +19,15 @@ switch (day) {
     break;
   case "day3":
     results = day3.process(fileLoc);
+    break;
+  case "day4":
+    results = day4.processData(fileLoc);
+    break;
+  case "day5":
+    day5.test_two();
+
+    results = day5.processData(fileLoc);
+    break;
   case undefined:
     console.log("No input given");
     break;
